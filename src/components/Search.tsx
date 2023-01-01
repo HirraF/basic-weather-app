8a91888254e69639c76756657902df411ee84430
HirraF <54152886+HirraF@users.noreply.github.com>
2023-01-01 15:05:20 +0000

    added interactivity to autocomplete searchbar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,18 +11,16 @@ type SearchProps = {
 function Search(props: SearchProps) {
     const [locationInput, setLocationInput] = useState('')
     const [locationData, setData] = useState<parsedLocation[]>(emptyLocation);
+    const [showResults, setShowResults] = useState(true);
 
+    // Text Input
     function handleChange(e: FormEvent<HTMLInputElement>) {
+        setShowResults(true);
         setLocationInput(e.currentTarget.value)
     }
 
-    function handleSubmit(e: SyntheticEvent) {
-        e.preventDefault();
-        props.setLocation(locationInput);
-    }
-
+    // Autocomplete
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=5&appid=${process.env.REACT_APP_APP_KEY}`
-
     useEffect(() => {
         if (locationInput !== '') {
             fetch(url)
@@ -38,20 +36,39 @@ function Search(props: SearchProps) {
 
     const createResults = (data: parsedLocation[]): ReactFragment | null => {
         if (data !== emptyLocation) {
-            return (data.map(location =>
-                <div> {location.name}, {location.state}, {location.country}</div>)
-            );
+            return (data.map((location, index) =>
+                <div key={index} data-key={index} onClick={handleListClick}>
+                    <div className="autocompleteItems" >
+                        {location.name}, {location.state}, {location.country}
+                    </div>
+                </div>
+            ));
         }
         return null;
+    }
+
+    function handleListClick(e: SyntheticEvent) {
+        e.preventDefault();
+        console.log(e.currentTarget.getAttribute('data-key'));
+        let locationKey = Number(e.currentTarget.getAttribute('data-key'));
+        let locationObj = locationData[locationKey];
+        let searchString = locationObj.name + ", " + locationObj.state + ", " + locationObj.country;
+        setLocationInput(searchString);
+        setShowResults(false);
+    }
+
+    function handleSubmit(e: SyntheticEvent) {
+        e.preventDefault();
+        props.setLocation(locationInput);
     }
 
     const results = createResults(locationData);
 
     return (
         <form>
-            <input type="text" name="location" onChange={handleChange} />
+            <input className="search" type="text" name="location" value={locationInput} onChange={handleChange} />
             <div className="autocomplete">
-                {results}
+                {showResults && results}
             </div>
             <input type="submit" value="Submit" onClick={handleSubmit} />
         </form>
